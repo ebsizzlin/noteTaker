977063f13ca72a16e7e2852996d9d3e9e5944c04
Ella Thompson <fang98t@gmail.com>
2020-11-22 23:27:39 -0500

    consolidating to server.js, was too confusing

--- server.js
+++ server.js
@@ -20,3 +20,25 @@ const htmlRoutes = require('routes/htmlRoutes');
 app.listen(PORT, function() {
     console.log('App listening on PORT: ' + PORT);
 });
+
+//tutor help
+const notes = JSON.parce(fs.readFileSync('db/db.json', 'utf8'));
+
+//module
+module.exports = function(app)  {
+    //get
+    app.get('/api/notes', function(req, res)    {
+        res.json(notes);
+    });
+
+    //post
+    app.post('/api/notes', function(req, res)   {
+        notes.push(req.body);
+        res.json('saved');
+    })
+
+    //delete
+    app.delete('api/notes/:id', function(req, res)  {
+
+    })
+}
