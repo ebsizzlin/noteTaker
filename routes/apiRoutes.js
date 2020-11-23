313f5081e38b2c588a128103ce16b966e42e43e3
Ella Thompson <fang98t@gmail.com>
2020-11-23 03:18:15 -0500

    removing extra pseudocode

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,58 +1,50 @@
 //require
 const fs = require('fs');
-const writeNotes = ('./db/db.json')
 
-module.exports = function(app)  {
+//start module
+module.exports = (app) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json', utf8));
+
     //get
-    app.get('/api/notes', function(req, res)    {
-        writeNotes
-            .newNotes()
-            .then(notes => res.json(notes))
-            .catch(err => res.status(500).json(err));
-            //status(500) is the error response, the "catch" is like notify of error
+    app.get('/api/notes', (req, res) => {
+        res.json(notes);
     });
 
     //post
-    app.post('/api/notes', function(req, res)   {
-        writeNotes
-            .addNote(req.body) //add note
-            .then((notes) => res.json(notes))
-            .catch(err => res.status(500).json(err));
+    app.post('/api/notes', (req, res) => {
+        var createNotes = req.body;
+        var noteData = (notes.length).toString();
+
+        createNotes.id = noteData;; //.id = unique identification
+        notes.push(createNotes);
+
+        //writeFile, stringify, throw errors
+        fs.writeFileSync('./db/db.json', JSON.stringify(notes), function(err)   {
+            if (err) throw (err);
+        });
+
+        //res the notes
+        res.json(notes);
     });
 
     //delete
-    app.delete('api/notes/:id', function(req, res)  {
-        writeNotes
-            .removeNote(req.params.id)
-            .then(() => res.json({ ok: true }))
-            .catch(err => res.status(500).json(err));
-    });
-};
+    app.delete('/api/notes/:id', function(req, res) {
+        var deleteNotes = req.params.id;
+        var noteNum = 0;
 
-// //module
-// module.exports = function(app)  {
-//     //get - complete
-//     app.get('/api/notes', function(req, res)    {
-//         const writeNotes = newNotes('./db/db.json', 'utf8', (err, data) =>   {
-//             if (err) throw err;
-//             return data
-//         })
-//         res.json(writeNotes);
-//     });
+        notes = notes.filter(current => { //.filter = creates an array
+            return current.id != deleteNotes;
+        });
 
-//     //post
-//     app.post('/api/notes', function(req, res)   {
-//         writeNotes.push(req.body);
-            //a lot missing here
-//         res.json(true);
-//     })
+        for (current of notes)  {
+            current.id = noteNum.toString();
+            noteNum++;
+        };
 
-//     //delete
-//     app.delete('api/notes/:id', function(req, res)  {
-//         writeNotes.length = 0;
-//         res.json({ ok: true });
-            //feels incomplete
-//     });
-// };
+        //writeFile, stringify
+        fs.writeFileSync('./db/db.json', JSON.stringify(notes));
 
-//use 'router' variable instead and export w module at the end?
+        //res the notes
+        res.json(notes);
+    })
+}
