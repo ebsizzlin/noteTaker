0dabc4c82355bbb93754667c67f7ec76ea61ae03
Ella Thompson <fang98t@gmail.com>
2020-11-22 23:37:04 -0500

    finalizing server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //require
 const express = require("express");
-const path = require("path");
-const fs = require('fs');
+// const path = require("path");
 
 //express
 const PORT = process.env.PORT || 3000;
@@ -12,29 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+//js require
+const apiRoutes = require('routes/apiRoutes');
+const htmlRoutes = require('routes/htmlRoutes');
+
 //listener
 app.listen(PORT, function() {
     console.log('App listening on PORT: ' + PORT);
 });
-
-//tutor help
-const notes = JSON.parce(fs.readFileSync('db/db.json', 'utf8'));
-
-//module
-module.exports = function(app)  {
-    //get
-    app.get('/api/notes', function(req, res)    {
-        res.json(notes);
-    });
-
-    //post
-    app.post('/api/notes', function(req, res)   {
-        notes.push(req.body);
-        res.json('saved');
-    })
-
-    //delete
-    app.delete('api/notes/:id', function(req, res)  {
-
-    })
-}
