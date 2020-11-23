2e5d55b4ba100e88df3d41e37ecd1692dd76c73b
Ella Thompson <fang98t@gmail.com>
2020-11-23 00:35:14 -0500

    apiRoutes getting rid of function, adding if/return

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,39 @@
 //require
 const fs = require('fs');
 const path = require('path');
-const data = require('../db/db.json', 'utf8'); //call json
+// const data = require('../db/db.json', 'utf8'); //call json
 
 //write function for json to callback (attempt)
-function write(notes)   {
-    notes = JSON.stringify(notes);
-    fs.writeFileSync("./db/db.json", notes, function(err)   {
-        if(err) {
-            return console.log(err);
-        };
-    });
-};
+// function write(notes)   {
+//     notes = JSON.stringify(notes);
+//     fs.writeFileSync("./db/db.json", notes, function(err)   {
+//         if(err) {
+//             return console.log(err);
+//         };
+//     });
+// };
 
 //module -- make this short and sweet
 module.exports = function(app)  {
     //get
     app.get('/api/notes', function(req, res)    {
-        res.json(notes);
+        const writeNotes = newNotes('./db/db.json', 'utf8', (err, data) =>   {
+            if (err) throw err;
+            return data
+        })
+        res.json(writeNotes);
     });
 
     //post
     app.post('/api/notes', function(req, res)   {
-        data.push(req.body);
-        write(notes)
-        res.json(notes);
+        writeNotes.push(req.body);
+        // write(notes)
+        res.json(true);
     })
 
     //delete
     app.delete('api/notes/:id', function(req, res)  {
-        notes.length = 0;
+        writeNotes.length = 0;
         res.json({ ok: true });
     });
 };
