b2a1dfb983d818eeb147ed710b90d700da0e9465
Ella Thompson <fang98t@gmail.com>
2020-11-22 23:35:59 -0500

    re-created route js files, found combined more confusing

---
+++ routes/apiRoutes.js
@@ -0,0 +1,25 @@
+//require
+const fs = require('fs');
+
+
+//tutor help
+const notes = JSON.parce(fs.readFileSync('db/db.json', 'utf8'));
+
+//module
+module.exports = function(app)  {
+    //get
+    app.get('/api/notes', function(req, res)    {
+        res.json(notes);
+    });
+
+    //post
+    app.post('/api/notes', function(req, res)   {
+        notes.push(req.body);
+        res.json('saved');
+    })
+
+    //delete
+    app.delete('api/notes/:id', function(req, res)  {
+
+    })
+}
