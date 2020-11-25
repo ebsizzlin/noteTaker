98425da9ed159bc83b7ea56fe9dea8e9a7490a34
Ella Thompson <fang98t@gmail.com>
2020-11-24 20:09:35 -0500

    app.delete not disappearing troubleshoot

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -137,7 +137,6 @@ const renderNoteList = (notes) => {
 
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => {
-  console.log('getting notes');
   return getNotes().then(renderNoteList);
 };
 
