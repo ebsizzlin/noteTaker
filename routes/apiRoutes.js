7867825dcdaf65b1c79875c58110409aaa87ce74
Ella Thompson <fang98t@gmail.com>
2020-11-24 19:43:40 -0500

    trying ? and null for app.delete, simplifying

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -32,23 +32,25 @@ module.exports = (app) => {
 
     //delete -- all help from BCS, review 11/21 class & async
     app.delete('/api/notes/:id', (req, res) => {
-        const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+        // const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
         var deleteNotes = req.params.id;
         //.params is a shortcut to use when calling a method, dont have to create an array
-        var noteNum = 0;
-        console.log(notes);
+        // var noteNum = 0;
+        // console.log(notes);
         notes = notes.filter(thisNote => {
             //.filter = creates a a new array that contaisn a subset of elements of a previous array -- here, it removes the note the user is deleting from the previous array
-            return thisNote.id !== deleteNotes;
+            return thisNote.id !== deleteNotes ? thisNote : null;
         });
 
-        for (thisNote of notes)  {
-            thisNote.id = noteNum.toString();
-            noteNum++;
-        };
+        // for (thisNote of notes)  {
+        //     thisNote.id = noteNum.toString();
+        //     noteNum++;
+        // };
 
         //writeFile, stringify
-        fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+        fs.writeFileSync('./db/db.json', JSON.stringify(notes), function(err)   {
+            if (err) throw (err);
+        });
 
         //res the notes
         res.json(notes);
